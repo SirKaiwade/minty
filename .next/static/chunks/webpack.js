a7b0878f358ebba2845726e6437cf9ab575a9f4e
Chris Okwakol <chrisokwakol@gmail.com>
2022-06-09 21:12:07 +0300

    Optimized banner for mobile

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e1011e747d070b5"; }
+/******/ 		__webpack_require__.h = function() { return "631cf11b0f57c1a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
