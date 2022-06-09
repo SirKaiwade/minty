dfd410276ef38c151e17593a17d67b331c20bb92
Chris Okwakol <chrisokwakol@gmail.com>
2022-06-09 21:27:53 +0300

    Margin top marquee title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "32780851a158093f"; }
+/******/ 		__webpack_require__.h = function() { return "ef9638222bc2aa45"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
